f2aa55e8cdd0c7aa2fdc9bfc28ad227e2c1d6365
esacdevelopment <54295143+esacdevelopment@users.noreply.github.com>
2019-10-06 19:01:56 -0500

    TESTING 1.3.1
    This is a testing release, not meant to be seen by the public.

--- scripts.js
+++ scripts.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
         }
     });
 });
-
 $(document).ready(function() {
     // Determine the navbar's background:
     var scrollHeight = $(document).scrollTop();
@@ -34,9 +33,10 @@ $(document).scroll(function(e) {
             $(".sd-nav.show-header").addClass('transparent');
         }
         // Parallax effect
-    if (parseInt($("body").css('width')) > 720) {
+        //parseInt($("body").css('width')) > 720 && -- might need to add to the following if statement
+    if (!$(".sd-page-header").hasClass("sd-no-parallax")) {
         var a = $(document).scrollTop() / 3;
-        $(".sd-page-header").css('background-position', '0px -' + a + 'px');
+        $(".sd-page-header").css('background-position', '0px ' + a + 'px');
     }
 });
 $(document).on('click', '.sd-nav button', function() {
@@ -52,8 +52,9 @@ $(document).ready(function() {
     $(".sd-nav-co").fadeOut(100);
     $(".sd-nav button:not(.sd-show-options)").fadeIn(1);
     if (buttonshow == false) {
-        $(".sd-nav button").each(function() {
+        $(".sd-nav button:not(.sd-show-options)").each(function() {
                 buttoncount++;
+                $(this).data('originaltext', $(this).html());
                 $(this).html("<i class='fa fa-" + $(this).data('icon') + "'></i>");
             buttonshow = true;
             $(".sd-nav button.sd-show-options i").css("transform", 'rotate(180deg)');
@@ -62,27 +63,8 @@ $(document).ready(function() {
             $(".sd-nav button:not(.sd-show-options)").fadeOut(1);
             $(".sd-nav .sd-nav-co").fadeIn(100);
             $(".sd-nav button.sd-show-options i").css('transform', 'rotate(0deg)');
-            $(".sd-nav button").each(function() {
-                switch ($(this).html().trim()) {
-                    case '<i class="fa fa-home"></i>':
-                        $(this).html('Home');
-                        break;
-                    case '<i class="fa fa-download"></i>':
-                        $(this).html('Download');
-                        break;
-                    case '<i class="fa fa-info"></i>':
-                        $(this).html('About');
-                        break;
-                    case '<i class="fa fa-user"></i>':
-                        $(this).html('Creator');
-                        break;
-                    case '<i class="fa fa-phone"></i>':
-                        $(this).html('Contact');
-                        break;
-                    case '<i class="fa fa-ellipsis-v"></i>':
-                        $(this).html('Examples');
-                        break;
-                }
+            $(".sd-nav button:not(.sd-show-options)").each(function() {
+                $(this).html($(this).data('originaltext'))
                 buttonshow = false;
             });
         }
