ce52bd99905b9f840bbfe4b72a0361d55e875a08
esacdevelopment <54295143+esacdevelopment@users.noreply.github.com>
2019-10-08 21:09:53 -0500

    Beta 1.5
    Added full functionality for a side bar on mobile. When user expands the navbar, the sidebar is displayed underneath. The sidebar is to include a search when one is specified by the developer. That feature is being worked on.

--- scripts.js
+++ scripts.js
@@ -12,6 +12,13 @@ $(document).ready(function () {
             console.error("SD Error: No property \"data-icon\" on a button element in the navigation bar");
         }
     });
+}).on('click', 'body > *:not(.sd-side-nav)', function (e) {
+    if (parseInt($('.sd-side-nav').css('left')) == 0) {
+        if (parseInt($('body').css('width')) > 720) {
+            $("body > *").css('filter', 'blur(0px)');
+            $(".sd-side-nav").css('left', '-100vw');
+        }
+    }
 });
 $(document).ready(function () {
     // Determine the navbar's background:
@@ -55,7 +62,8 @@ $(document).ready(function () {
     $(".sd-nav").css('max-height', '25vh').css('height', '25vh').addClass('js-nav-open').children('.sd-show-options').css('height', '50%');
     $(".sd-nav button").css('display', 'inline-block');
     if (buttonshow === false) {
-        $(".sd-nav button:not(.sd-show-options)").each(function () {
+        showsidebarmobile();
+        $(".sd-nav button:not(.sd-show-options):not(.sd-show-side-nav)").each(function () {
             buttoncount++;
             $(this).data('originaltext', $(this).html());
             if (!$(this).hasClass("sd-show-options")) {
@@ -66,16 +74,20 @@ $(document).ready(function () {
         $(".sd-nav button.sd-show-options i").css('transform', 'rotate(180deg)');
         $(".sd-nav button:not(.sd-show-options)").css('width', 'calc(100vw / (' + buttoncount + ')');
     } else {
-        $(".sd-nav a.sd-nav-co").css('line-height', '10vh');
-        $(".sd-nav").css('max-height', '10vh').css('height', '10vh').removeClass('js-nav-open').children('button').css('height', '100%');
-        $(".sd-nav button:not(.sd-show-options)").css('display', 'none');
-        $(".sd-nav .sd-nav-co").fadeIn(100);
-        $(".sd-nav button.sd-show-options i").css('transform', 'rotate(0deg)');
-        $(".sd-nav button:not(.sd-show-options)").each(function () {
-            $(this).html($(this).data('originaltext'))
-            buttonshow = false;
-        });
-        $(".sd-nav button.sd-show-options").css('width', '30%').css('height', '100%');
+        hidesidebarmobile();
+        function waitasec() {
+            $(".sd-nav a.sd-nav-co").css('line-height', '10vh');
+            $(".sd-nav").css('max-height', '10vh').css('height', '10vh').removeClass('js-nav-open').children('button').css('height', '100%');
+            $(".sd-nav button:not(.sd-show-options)").css('display', 'none');
+            $(".sd-nav .sd-nav-co").fadeIn(100);
+            $(".sd-nav button.sd-show-options i").css('transform', 'rotate(0deg)');
+            $(".sd-nav button:not(.sd-show-options)").each(function () {
+                $(this).html($(this).data('originaltext'))
+                buttonshow = false;
+            });
+            $(".sd-nav button.sd-show-options").css('width', '30%').css('height', '100%');
+        }
+        setTimeout(waitasec, 300);
     }
     $(".sd-nav").css('width', '100%');
 });
@@ -94,3 +106,38 @@ $(document).on('click', '.sd-side-nav-wrapper', function () {
         // Close the navbar
     }
 });
+// Side Nav for desktop
+$(document).on('click', '.sd-nav button.sd-show-side-nav', function () {
+    $(".sd-side-nav").css('left', '0');
+    $("body > *").css('filter', 'blur(5px) grayscale(100%)');
+    $("body > .sd-side-nav").css('filter', 'none');
+});
+function showsidebarmobile() {
+    $(".sd-side-nav").css('width', '100vw')
+        .css('height', '75vh')
+        .css('background', '#dddddd')
+        .css('top', '-100vh')
+        .css('z-index', '9999');
+    function iminside() {
+        $(".sd-side-nav").css('left', '0');
+    }
+    setTimeout(iminside, 200);
+    function anotherone() {
+        $(".sd-side-nav").css('top', '25vh');
+    }
+    setTimeout(anotherone, 400);
+}
+function hidesidebarmobile() {
+    $(".sd-side-nav").css('top', '-1000vh');
+    function waitformetohide() {
+        $(".sd-side-nav").css('left', '-100vw')
+            .css('height', '100vh')
+            .css('background', '#fff')
+            .css('z-index', '100000');
+    }
+    setTimeout(waitformetohide, 300);
+    function manthisisialotoffuncctions() {
+        $('.sd-side-nav').css('top', '0');
+    }
+    setTimeout(manthisisialotoffuncctions, 600);
+}
