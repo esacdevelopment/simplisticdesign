745ce37ce51ac4934767841de52a1be68daccc76
isaacmaddox <54295143+esacdevelopment@users.noreply.github.com>
2019-10-29 08:29:51 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
             }
         });
     });
-}).on('click', '*:not(.sd-side-nav):not(.sd-dialog)', function (e) {
+}).on('click', '*:not(.sd-side-nav):not(.sd-dialog), article', function (e) {
     if (parseInt($('.sd-side-nav').css('left')) == 0) {
         if (parseInt($('body').css('width')) > 720) {
             $("body > *").css('filter', 'blur(0px)');
